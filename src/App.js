75a3d0961573c24b7b7414a76b59775ce89a155b
mrozo2m <mrozo2m@interia.pl>
2019-04-19 09:23:40 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { set, getState } from './store'
+
+set('users', [])
+
+const App = (props) => (
+  <div>
+    
+  </div>
+)
+
+export default App
---
+++ src/store.js
@@ -0,0 +1,8 @@
+const store = {}
+
+export const getState = () => store
+
+export const set = (key, value) => store[key] = value
+
+export const get = (key) => store[key]
+
